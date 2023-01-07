22a88e364a1e513ee341ae5295141dbc04890dc1
Nobady912 <mamatietie@gmail.com>
2023-01-07 16:23:51 -0700

    c

---
+++ Forecasting: Principles and Practice/chapter 4 time series decomposition.R
@@ -0,0 +1,35 @@
+# The time series decomposition
+  #use it when there are no abailablity data
+  # data are available, statisc forecasts are generated, 
+  #and then adjustment using judgment
+  #data are available and statistical and judgmental
+    #forecasts are generated independently and then combined
+
+#4.2 the key principles 
+  #Set the forecasting task clearly and concisely
+  #lmplement a systematic approach
+  #document and ustift
+  #systemically ealuate forecasts
+  #segregate orecasters and user.
+
+#4.3 The delphi method
+  #A panel of experts is assembled.
+  #Forecasting tasks/challenges are set and distributed to the experts.
+  #Experts return initial forecasts and justifications. These are compiled and
+    #summarised in order to provide feedback.
+  #Feedback is provided to the experts, who now review their forecasts 
+    #in light of the feedback. This step may be iterated until a satisfactory level of consensus is reached.
+  #Final forecasts are constructed by aggregating the experts’ forecasts.
+
+#4.4 forecasting by analogy 类比预测
+  #A useful judgmental approach which is often implemented in practice is forecasting by analogy. 
+  #A common example is the pricing of a house through an appraisal process.
+
+#4.5 Scenario forecasting 情景预测
+#4.6 new product forecasting 新产品预测
+  #sale force composite
+  #executive opinion
+  #Customer intentions
+
+#4.7judgenet adjustment 
+  $
