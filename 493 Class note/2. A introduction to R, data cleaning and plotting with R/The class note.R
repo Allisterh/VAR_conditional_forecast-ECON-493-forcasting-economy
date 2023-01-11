b5f5bb5de73f2f4290e390c860cd262ec6e89b36
Nobady912 <mamatietie@gmail.com>
2023-01-11 10:45:09 -0700

    down for the first class! god, so many things need to do 2023/01/11

--- 493 Class note/2. A introduction to R, data cleaning and plotting with R/The class note.R
+++ 493 Class note/2. A introduction to R, data cleaning and plotting with R/The class note.R
@@ -1,6 +1,7 @@
 install.packages("tidyverse")
 install.packages("lmtest")
 library("tidyverse")
+library("ggplot2")
 
 #the tip
   #i wantto have the same bersion of hte software as my student
@@ -27,3 +28,64 @@ library("tidyverse")
 #most data sets you will work with will be road into R and stored as dataframe
 # so this course will mainly focus on ,mainpulating and visulaizing these objets. 
 
+#The the same priciple  can be used to create more complsx tobjects lie matrix, arrys list anda datafreamas
+#most data sets you will work with will be read into and stored as data frame so this cource will ianly foucisng
+
+#so, other wat of rreathing obejctive, you have a lot of tip for creatinga lot of thing, 
+
+# you need to save it. 
+
+#save(The class note, file = "wulala gei")
+# this thing need to using the save the enviroment it can help on saivng both the script. 
+
+data(cars)
+View(cars)
+
+#strs 
+  #Can tell you which kind of data you got.
+# you can create a data
+# when you looking at the table. 
+cars2 <- as_tibble(cars)
+head(cars2)
+summary(cars)
+
+# The histgrame
+# how to access only one bvairlce
+
+str(cars$speed)
+# if you put an asisgnment suchas x<-y in parenteses (), R will print the outut of hte assignmnet out for tou in our document.
+
+#Linear regress ion 
+fit <- lm(dist ~ speed, data = cars)
+summary(fit)
+
+str(fit)
+
+#another ugly ploy
+plot(cars$speed, cars$dist)
+abline()
+
+#a nicer ploy with ggplots
+ggplot(data = cars, aes(x = speed, y = dist)) + geom_point() + geom_smooth(method = "lm")
+
+#working with acutally data
+  #using the function of read.cvs
+
+#hwo to rea it
+  #package foreign
+    #part of base R
+  #package haven
+    # paryt of the tidyverse family
+
+#the data set miderml.cvs
+#th header = false/ture 
+  #the first linke of the first code is eqmpty or not.
+
+#na_rm = ture
+  #remove the missing the value form the varble. 
+
+#coord_cartestian ()  
+  #set the grade size that you need to do. 
+
+
+#clanfear.github.io/Csss508
