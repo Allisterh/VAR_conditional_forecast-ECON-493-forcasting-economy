9d6584dfdc25fdca2c1280314461e0b5976e10d0
Nobady912 <mamatietie@gmail.com>
2023-01-11 10:12:52 -0700

    w

--- 493 Class note/2. A introduction to R, data cleaning and plotting with R/The class note.R
+++ 493 Class note/2. A introduction to R, data cleaning and plotting with R/The class note.R
@@ -6,3 +6,24 @@ library("tidyverse")
   #i wantto have the same bersion of hte software as my student
   #bUT I DO'T WANT AN UPDATE TO break anything
   # do not update anything until you are down.
+
+# squrt(400)
+
+#An object's name represet tthe information store in that object, so you can treat the obect's name as it it were the vaule stored inside
+
+#An vestoryt is seris of elements such as numbers, you can cereate a vector and store it as an obejct in the same way. 
+#the function can do the al the timne.
+
+
+# we ofthen work with. dat that are cartegorical, to created a vector of rtest - strings in programming term
+  #we must place the txt in quotes
+  #string vecotr
+
+#categrica data can aso bes stored as a factor, which has a underlying nunmberic
+  #presentation, model witll convert factors to dummies
+  #factor? 
+
+#the same priples can be used tp. create more complex obejct like matrices, arrys lists and daterframe
+#most data sets you will work with will be road into R and stored as dataframe
+# so this course will mainly focus on ,mainpulating and visulaizing these objets. 
+
