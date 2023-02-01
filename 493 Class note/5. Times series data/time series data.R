624c7deabadd69633a83a506b911e8a25f79e162
Nobady912 <mamatietie@gmail.com>
2023-02-01 10:17:17 -0700

    v

---
+++ 493 Class note/5. Times series data/time series data.R
@@ -0,0 +1,23 @@
+#Step one Lode the all package that necessary. 
+
+library ("lubridate")    
+library ("cansim")       
+library ("OECD")        
+library ("WDI")          
+library ("fredr")        
+library ("tsbox")
+library ("RColorBrewer")
+library("wesanderson")
+library("writexl")
+library("ggplot2")
+library("fpp2")
+library("glmnet")
+library("mosaicCalc")
+library("tidyr")
+library("lmtest")
+library("boot")
+
+###########
+#you build the model and the model wullhave some redisula diagnostics 
+  #
+
